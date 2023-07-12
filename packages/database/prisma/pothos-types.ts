5c4f8b1e53dc6e55bf7d8398d0ec5fb4df630d9c
Brad Menchl <brad.menchl@classdojo.com>
2023-07-12 16:09:50 -0400

    idk

--- packages/database/prisma/pothos-types.ts
+++ packages/database/prisma/pothos-types.ts
@@ -1,96 +1,102 @@
 /* eslint-disable */
-import type { Prisma, Movie, Friend, Night, MovieSelection } from "@prisma/client";
+import type {
+  Prisma,
+  Movie,
+  Friend,
+  Night,
+  MovieSelection,
+} from '@prisma/client';
 export default interface PrismaTypes {
-    Movie: {
-        Name: "Movie";
+  Movie: {
+    Name: 'Movie';
+    Shape: Movie;
+    Include: Prisma.MovieInclude;
+    Select: Prisma.MovieSelect;
+    OrderBy: Prisma.MovieOrderByWithRelationInput;
+    WhereUnique: Prisma.MovieWhereUniqueInput;
+    Where: Prisma.MovieWhereInput;
+    Create: {};
+    Update: {};
+    RelationName: 'movieSelections';
+    ListRelations: 'movieSelections';
+    Relations: {
+      movieSelections: {
+        Shape: MovieSelection[];
+        Name: 'MovieSelection';
+      };
+    };
+  };
+  Friend: {
+    Name: 'Friend';
+    Shape: Friend;
+    Include: Prisma.FriendInclude;
+    Select: Prisma.FriendSelect;
+    OrderBy: Prisma.FriendOrderByWithRelationInput;
+    WhereUnique: Prisma.FriendWhereUniqueInput;
+    Where: Prisma.FriendWhereInput;
+    Create: {};
+    Update: {};
+    RelationName: 'movieSelections';
+    ListRelations: 'movieSelections';
+    Relations: {
+      movieSelections: {
+        Shape: MovieSelection[];
+        Name: 'MovieSelection';
+      };
+    };
+  };
+  Night: {
+    Name: 'Night';
+    Shape: Night;
+    Include: Prisma.NightInclude;
+    Select: Prisma.NightSelect;
+    OrderBy: Prisma.NightOrderByWithRelationInput;
+    WhereUnique: Prisma.NightWhereUniqueInput;
+    Where: Prisma.NightWhereInput;
+    Create: {};
+    Update: {};
+    RelationName: 'movieSelections' | 'winningSelection';
+    ListRelations: 'movieSelections';
+    Relations: {
+      movieSelections: {
+        Shape: MovieSelection[];
+        Name: 'MovieSelection';
+      };
+      winningSelection: {
+        Shape: MovieSelection | null;
+        Name: 'MovieSelection';
+      };
+    };
+  };
+  MovieSelection: {
+    Name: 'MovieSelection';
+    Shape: MovieSelection;
+    Include: Prisma.MovieSelectionInclude;
+    Select: Prisma.MovieSelectionSelect;
+    OrderBy: Prisma.MovieSelectionOrderByWithRelationInput;
+    WhereUnique: Prisma.MovieSelectionWhereUniqueInput;
+    Where: Prisma.MovieSelectionWhereInput;
+    Create: {};
+    Update: {};
+    RelationName: 'movie' | 'friend' | 'night' | 'winningNight';
+    ListRelations: never;
+    Relations: {
+      movie: {
         Shape: Movie;
-        Include: Prisma.MovieInclude;
-        Select: Prisma.MovieSelect;
-        OrderBy: Prisma.MovieOrderByWithRelationInput;
-        WhereUnique: Prisma.MovieWhereUniqueInput;
-        Where: Prisma.MovieWhereInput;
-        Create: {};
-        Update: {};
-        RelationName: "movieSelections";
-        ListRelations: "movieSelections";
-        Relations: {
-            movieSelections: {
-                Shape: MovieSelection[];
-                Name: "MovieSelection";
-            };
-        };
-    };
-    Friend: {
-        Name: "Friend";
+        Name: 'Movie';
+      };
+      friend: {
         Shape: Friend;
-        Include: Prisma.FriendInclude;
-        Select: Prisma.FriendSelect;
-        OrderBy: Prisma.FriendOrderByWithRelationInput;
-        WhereUnique: Prisma.FriendWhereUniqueInput;
-        Where: Prisma.FriendWhereInput;
-        Create: {};
-        Update: {};
-        RelationName: "movieSelections";
-        ListRelations: "movieSelections";
-        Relations: {
-            movieSelections: {
-                Shape: MovieSelection[];
-                Name: "MovieSelection";
-            };
-        };
-    };
-    Night: {
-        Name: "Night";
+        Name: 'Friend';
+      };
+      night: {
         Shape: Night;
-        Include: Prisma.NightInclude;
-        Select: Prisma.NightSelect;
-        OrderBy: Prisma.NightOrderByWithRelationInput;
-        WhereUnique: Prisma.NightWhereUniqueInput;
-        Where: Prisma.NightWhereInput;
-        Create: {};
-        Update: {};
-        RelationName: "movieSelections" | "winningSelection";
-        ListRelations: "movieSelections";
-        Relations: {
-            movieSelections: {
-                Shape: MovieSelection[];
-                Name: "MovieSelection";
-            };
-            winningSelection: {
-                Shape: MovieSelection | null;
-                Name: "MovieSelection";
-            };
-        };
+        Name: 'Night';
+      };
+      winningNight: {
+        Shape: Night | null;
+        Name: 'Night';
+      };
     };
-    MovieSelection: {
-        Name: "MovieSelection";
-        Shape: MovieSelection;
-        Include: Prisma.MovieSelectionInclude;
-        Select: Prisma.MovieSelectionSelect;
-        OrderBy: Prisma.MovieSelectionOrderByWithRelationInput;
-        WhereUnique: Prisma.MovieSelectionWhereUniqueInput;
-        Where: Prisma.MovieSelectionWhereInput;
-        Create: {};
-        Update: {};
-        RelationName: "movie" | "friend" | "night" | "winningNight";
-        ListRelations: never;
-        Relations: {
-            movie: {
-                Shape: Movie;
-                Name: "Movie";
-            };
-            friend: {
-                Shape: Friend;
-                Name: "Friend";
-            };
-            night: {
-                Shape: Night;
-                Name: "Night";
-            };
-            winningNight: {
-                Shape: Night | null;
-                Name: "Night";
-            };
-        };
-    };
+  };
 }
