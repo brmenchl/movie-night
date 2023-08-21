3d0956c26f1c5981d41e661a99044e9c8a5c2705
Brad Menchl <brad.menchl@classdojo.com>
2023-08-21 03:06:26 +0000

    get rid of redirect

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -9,11 +9,4 @@ module.exports = {
   eslint: {
     dirs: ['pages', 'screens', 'packages', 'core', 'components'],
   },
-  async redirects() {
-    return [{
-      source: '/',
-      destination: '/nights/d09e8648-ef97-4959-ae0c-fb12c4c82f9d',
-      permanent: false,
-    }]
-  }
 };
