2e90fbc0c5b3ed8c2596242a18d5f0587b625837
Brad Menchl <brmenchl@gmail.com>
2024-06-15 22:17:21 +0000

    fix type for prisma client

--- packages/server/builder.ts
+++ packages/server/builder.ts
@@ -1,8 +1,7 @@
 import SchemaBuilder from '@pothos/core';
 import PrismaPlugin from '@pothos/plugin-prisma';
 import WithInputPlugin from '@pothos/plugin-with-input';
-import { prismaClient } from 'database';
-import type { PrismaTypes } from 'database';
+import { type PrismaTypes, prismaClient } from 'database';
 
 const builder = new SchemaBuilder<{ PrismaTypes: PrismaTypes }>({
   plugins: [PrismaPlugin, WithInputPlugin],
